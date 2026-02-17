e970762f1659266bf96f33bf50cfd78a1855c1ee
vtu24669-crypto <vtu24669@veltech.edu.in>
2026-02-17 09:21:52 +0530

    Time: 4 ms (70.73%) | Memory: 49.9 MB (40.13%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -0,0 +1,19 @@
+class Solution {
+    List<String> res = new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root == null) return res;
+        getAllPaths(root, "");
+        return res;
+    }
+    void getAllPaths(TreeNode node, String path){
+        if(node == null) return;
+
+        if(node.left == null && node.right == null){
+            res.add(path + node.val);
+            return;
+        }
+
+        getAllPaths(node.left, path + node.val + "->");
+        getAllPaths(node.right, path + node.val + "->");
+    }
+}
