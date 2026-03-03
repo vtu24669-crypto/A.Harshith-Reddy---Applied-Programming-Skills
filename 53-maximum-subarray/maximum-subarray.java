980fb4d8ad4e64f57f8a12ac9df2901361659b0f
vtu24669-crypto <vtu24669@veltech.edu.in>
2026-03-03 11:07:22 +0530

    Time: 1 ms (99.75%) | Memory: 77.4 MB (47.35%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = nums[0];
+        
+        for(int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            maxSum = Math.max(currentSum, maxSum);
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
